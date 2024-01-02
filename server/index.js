e7d1086cd5f3eef939ad8f8861c01cbff4a85ac5
Arvind Date <84241308+arvinddate@users.noreply.github.com>
2024-01-03 01:12:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,24 +1,22 @@
 import { config } from "dotenv";
 import express from "express";
-import path from 'path';
 import connectToDB from "./config/dBconfig.js";
 import userRouter from './routes/userRoutes.js';
 import MovieRoutes from './routes/movieRouter.js';
 import TheatreRouter from './routes/theatreRouter.js';
 import BookinghRouter from './routes/bookingRoutes.js';
 
-import { fileURLToPath } from 'url';
-import { dirname ,join} from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-
 import cors from 'cors';
 const app=express();
 app.use(express.json());
 app.use( cors());
 config();
-app.use(express.static(path.join(__dirname, 'Book_My_Shows/client/build')));
+app.use('/', express.static('public'));
+
+app.use('*', (req, res) => {
+    res.status(400).send('Page not found!');
+});
+
 app.use('/api/user',userRouter);
 app.use('/api/movie',MovieRoutes);
 app.use("/api/theatre",TheatreRouter );
