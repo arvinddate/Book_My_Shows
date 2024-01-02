7ff5fd51f0d2698898b9d707db447bcbd65a9c2a
Arvind Date <84241308+arvinddate@users.noreply.github.com>
2024-01-03 01:30:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,20 +9,22 @@ import BookinghRouter from './routes/bookingRoutes.js';
 import cors from 'cors';
 const app=express();
 app.use(express.json());
+app.use(express.static('public'));
+
 app.use( cors());
 config();
+
+
+app.use('/api/user',userRouter);
+app.use('/api/movie',MovieRoutes);
+app.use("/api/theatre",TheatreRouter );
+app.use("/api/bookings",BookinghRouter );
+
 app.use('/', express.static('public'));
 
 app.use('*', (req, res) => {
     res.status(400).send('Page not found!');
 });
-
-app.use('/api/user',userRouter);
-app.use('/api/movie',MovieRoutes);
-app.use("/api/theatre",TheatreRouter );
- app.use("/api/bookings",BookinghRouter );
-
-
 app.listen(5050, async() => {
     await connectToDB()
     console.log(`Server started on port`);
